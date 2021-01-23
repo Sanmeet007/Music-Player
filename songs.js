e45378d9309b328f8492af4191cb0f8998ffed33
Sanmeet007 <77886304+Sanmeet007@users.noreply.github.com>
2021-01-23 22:48:00 +0530

    Songs

---
+++ songs.js
@@ -0,0 +1,88 @@
+var songs_playlist =[
+  {
+  name:   "Havana" , 
+  poster: "images/havana.jpg" , 
+  src :   "music/havana.mp3" ,
+  subtitles : "Subs/havana.vtt" ,
+  artist: "Camila Cabello"
+  },
+  {
+  name:   "Warriyo - Mortals " , 
+  poster: "images/warriyo.jpg" , 
+  src :   "music/warriyo.mp3" ,
+  subtitles : "subs/warriyo.vtt" ,
+  artist: "feat. Laura Brehm"
+  },
+  {
+    name:   "Make Me move" , 
+  poster: "images/makemove.jpg" , 
+  src :   "music/makemove.mp3" ,
+  subtitles : "subs/makemve.vtt" ,
+  artist: "Feat. Karra"
+  },
+  {
+    name:   "Girls Like You " , 
+  poster: "images/girlslikeu.jpg" , 
+  src :   "music/girlslikeu.mp3" ,
+  subtitles : "subs/girlslikeu.vtt" ,
+  artist: "Maroon 5"
+  },
+  {
+    name:   "Faded" , 
+  poster: "images/faded.jpg" , 
+  src :   "music/faded.mp3" ,
+  subtitles : "subs/faded.vtt" ,
+  artist: "Alan Walker"
+  },
+  {
+    name:   "Señorita" , 
+  poster: "images/senorita.jpg" , 
+  src :   "music/senorita.mp3" ,
+  subtitles : "subs/senorita.vtt" ,
+  artist: "Camila Cabello and Shawn Mendes"
+  },
+  {
+    name:   "hero too" , 
+  poster: "images/myhero.jpg" , 
+  src :   "music/myhero.mp3" ,
+  subtitles : "subs/myhero.vtt" ,
+  artist: "Chrissy Costanza"
+  },
+  {
+    name:   "Shinzo wo Sasageyo!" , 
+  poster: "images/aot-theme.jpg" , 
+  src :   "music/aot-theme.mp3" ,
+  subtitles : "subs/" ,
+  artist: "By Linked Horizon"
+  },
+  {
+    name:   "Attack on Titan" , 
+  poster: "images/aot5.jpg" , 
+  src :   "music/aot5.mp3" ,
+  subtitles : "subs/" ,
+  artist: " Original Soundtrack"
+  },
+  {
+    name:   "HERO ONE PUNCH MAN" , 
+  poster: "images/opm.jpg" , 
+  src :   "music/opm.mp3" ,
+  subtitles : "subs/opm.vtt" ,
+  artist: " JAM Project"
+  },
+  {
+    name:   "Pray for me" , 
+  poster: "images/prayforme.jpg" , 
+  src :   "music/prayforme.mp3" ,
+  subtitles : "subs/prayforme.vtt" ,
+  artist: "Kendrick Lamar and The Weeknd"
+  },
+   
+   {
+     name:   "On & On" , 
+  poster: "images/cartoon.jpg" , 
+  src :   "music/cartoon.mp3" ,
+  subtitles : "subs/cartoon.vtt" ,
+  artist: "By Cartoon feat. Daniel Levi"
+  }
+  
+];
